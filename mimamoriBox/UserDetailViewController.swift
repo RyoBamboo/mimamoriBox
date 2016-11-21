1b4ef615978960cea4b64b8df9837e68c846e14b
竹之下遼 <itakedaka@gmail.com>
2016-11-21 13:45:31 +0900

    背景画像を設定

--- mimamoriBox/UserDetailViewController.swift
+++ mimamoriBox/UserDetailViewController.swift
@@ -7,9 +7,16 @@ import UIKit
 
 class UserDetailViewController: UIViewController {
     
+    var user: User! // リストで選択されたユーザ
+    
+    @IBOutlet weak var thumbnail: UserImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 画面の初期化、テーブルビュー選択時にuser変数が渡ってくる
+        viewInit()
         
         
     }
@@ -17,6 +24,12 @@ class UserDetailViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // 画面の初期化
+    func viewInit() {
+        self.thumbnail?.sd_setImageWithURL(NSURL(string: "http://okame.prodrb.com/img/" + user.imgPath))
+        self.userNameLabel.text = user.name
     }
     
     
